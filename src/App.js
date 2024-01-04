71215f83ab50351bee1e96eddc4aef5bcdd82f9f
ojingwaDaniel <ojingwadanny@gmail.com>
2024-01-04 16:12:50 +0100

    added two component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Nav from './component/nav';
+import Main from './component/main';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Nav/>
+
+
+      <Main/>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    </header> */}
     </div>
   );
 }
---
+++ src/component/nav.js
@@ -0,0 +1,7 @@
+export default function Nav(){
+    return(
+        <div>
+            <h1>Nav Bar goes here</h1>
+        </div>
+    )
+}
