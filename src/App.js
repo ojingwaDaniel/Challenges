6cc3475b29a0fd3bd8fc4219c3c50908d1bd2e43
ojingwaDaniel <ojingwadanny@gmail.com>
2024-01-04 17:04:03 +0100

    added the styling of the nsv bar component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// import logo from './logo.svg';
 import './App.css';
 import Nav from './component/nav';
 import Main from './component/main';
@@ -10,9 +9,6 @@ function App() {
 
 
       <Main/>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-    </header> */}
     </div>
   );
 }
--- src/component/nav.js
+++ src/component/nav.js
@@ -1,7 +1,13 @@
+import Logo from './reactjs-icon.png'
 export default function Nav(){
     return(
-        <div>
-            <h1>Nav Bar goes here</h1>
-        </div>
+        <header className="header">
+           <div className='reactfact'>
+            <img src={Logo} alt='NoImage'/>
+            <h1>ReactFacts</h1>
+            </div>
+            <h1 className='react'>React Course-Project 1</h1>  
+               
+        </header>
     )
 }
